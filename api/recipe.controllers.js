a9b1499853c7c007328cc4798bcd4954bb7e3d64
DannyBoyNYC <daniel.deverell@gmail.com>
2019-07-15 16:14:14 -0400

    review

--- api/recipe.controllers.js
+++ api/recipe.controllers.js
@@ -18,6 +18,7 @@ exports.add = function(req, res) {
 };
 
 exports.update = function() {};
+
 exports.delete = function(req, res) {
   let id = req.params.id;
   Recipe.remove({ _id: id }, result => {
@@ -38,29 +39,29 @@ exports.import = function(req, res) {
       title: 'Lasagna',
       description:
         'Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.',
-      image: 'lasagna.png',
+      image: 'lasagna.png'
     },
     {
       title: 'Pho-Chicken Noodle Soup',
       description:
         'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
-      image: 'pho.png',
+      image: 'pho.png'
     },
     {
       title: 'Guacamole',
       description:
         'Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.',
-      image: 'guacamole.png',
+      image: 'guacamole.png'
     },
     {
       title: 'Hamburger',
       description:
         'A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.',
-      image: 'hamburger.png',
+      image: 'hamburger.png'
     },
     function(err) {
       if (err) return console.log(err);
       return res.sendStatus(202);
-    },
+    }
   );
 };
