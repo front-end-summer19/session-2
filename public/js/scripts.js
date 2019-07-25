a5a25e6a86daee0c3e2c2affb01efdfb2e9ed1d9
DannyBoyNYC <daniel.deverell@gmail.com>
2019-07-25 15:13:26 -0400

    done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,95 @@
-fetch(`api/recipes`)
-.then(response => response.json())
-.then(recipes => renderStories(recipes));
+const homepage = () => {
+  fetch(`api/recipes`)
+  .then(response => response.json())
+  .then(recipes => renderStories(recipes));
+  
+  const renderStories = recipes => {
+    // console.log(recipes);
+    recipes.forEach(recipe => {
+      recipeEl = document.createElement('div');
+      recipeEl.innerHTML = `
+  <img src="img/${recipe.image}" />
+  <h3>${recipe.title}</h3>
+  <p>${recipe.description}</p>
+  <h4>Ingredients</h4>
+  <ul>
+    ${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+  </ul>
+  <h4>Preparation</h4>
+  <ul>
+    ${recipe.preparation.map(prep => `<li>${prep.step}</li>`).join('')}
+  </ul>
+  <a href="/">Back</a>
+  `;
+      document.querySelector('#root').append(recipeEl);
+    });
+    
+    const deleteBtns = document.querySelectorAll('.del');
+    // console.log(deleteBtns);
+    const delBtns = Array.from(deleteBtns);
+    // console.log('delBtns', delBtns);
+    delBtns.forEach(btn => {
+      btn.addEventListener('click', e => {
+        fetch(`api/recipes/${btn.dataset.id}`, {
+          method: 'DELETE'
+        });
+        e.preventDefault();
+        location.reload();
+      });
+    });
+    
+  }
+}
 
-const renderStories = recipes => {
-  // console.log(recipes);
-  recipes.forEach(recipe => {
+const detail = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const recipeId = urlParams.get('recipe');
+  console.log(recipeId);
+  fetch(`api/recipes/${recipeId}`)
+    .then(response => response.json())
+    .then(recipe => renderStory(recipe));
+  // NEW
+  const renderStory = recipe => {
+    console.log(recipe);
     recipeEl = document.createElement('div');
     recipeEl.innerHTML = `
-    <img src="img/${recipe.image}" />
-    <h3>${recipe.title}</h3>
-    <p>${recipe.description}</p>
-    <a class="del" data-id=${recipe._id} href="#">Delete</a>
-    `;
+      <img src="img/${recipe.image}" />
+      <h3>${recipe.title}</h3>
+      <p>${recipe.description}</p>
+      <a href="/">Back</a>
+      `;
     document.querySelector('#root').append(recipeEl);
-  });
 
-  const deleteBtns = document.querySelectorAll('.del');
-  // console.log(deleteBtns);
-  const delBtns = Array.from(deleteBtns);
-  console.log('delBtns', delBtns);
-  delBtns.forEach(btn => {
-    btn.addEventListener('click', e => {
-      fetch(`api/recipes/${btn.dataset.id}`, {
-        method: 'DELETE'
-      });
-      e.preventDefault();
-      location.reload();
-    });
-  });
+    const editForm = document.querySelector('form');
+    editForm.title.value = recipe.title;
+    editForm.image.value = recipe.image;
+    editForm.description.value = recipe.description;
+    // console.log(editForm.description);
+  };
+  // END NEW
+};
 
+
+
+const updateRecipe = () => {
+  const editForm = document.querySelector('form');
+  const urlParams = new URLSearchParams(window.location.search);
+  const recipeId = urlParams.get('recipe');
+
+  const updatedRecipe = {
+    title: editForm.title.value,
+    image: editForm.image.value,
+    description: editForm.description.value
+  };
+
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(updatedRecipe),
+    headers: { 'Content-Type': 'application/json' }
+  };
+
+  fetch(`api/recipes/${recipeId}`, options)
+    .then(response => console.log(response))
+    .then(() => location.reload()),
+    event.preventDefault();
 };
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const recipeModel = require('./api/recipe.model');
 const recipeControllers = require('./api/recipe.controllers');
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.static('public'));
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 
-const dataBaseURL =
-  'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
+const dataBaseURL = process.env.DATABASE;
+// const dataBaseURL =
+//   'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -23,12 +27,14 @@ app.put('/api/recipes/:id', recipeControllers.update);
 app.delete('/api/recipes/:id', recipeControllers.delete);
 app.get('/api/import', recipeControllers.import);
 app.get('/api/killall', recipeControllers.killall);
+app.post('/api/upload', recipeControllers.upload);
 
-const PORT = process.env.PORT || 3000;
+
 
 mongoose
   .connect(dataBaseURL, { useNewUrlParser: true })
   .then(() => console.log('MongoDb connected'))
   .catch(err => console.log(err));
 
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
